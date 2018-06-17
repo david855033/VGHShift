57acabf89c4a0c516ff88369f260217714112ab8
Lo Yu-Cheng <david855033@gmail.com>
2018-06-18 00:35:41 +0800

    .

--- src/components/querier/sheet-query.vue
+++ src/components/querier/sheet-query.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>empty</div>
+    <div>
+        <div id="sheet-selector" v-if="selectedSheetID==''">
+            <h4>Sheet Select (publish sheet)</h4>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>sheet_id</th>
+                        <th>year</th>
+                        <th>month</th>
+                        <th>section</th>
+                        <th>version</th>
+                        <th>status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(sheet,i) in sheetQueryResult" :key="i">
+                        <td>{{sheet.sheet_id}}</td>
+                        <td>{{sheet.year}}</td>
+                        <td>{{sheet.month}}</td>
+                        <td>{{sheet.section}}</td>
+                        <td>{{sheet.version}}</td>
+                        <td>{{sheet.status}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
-    
-}
+  data() {
+    return {
+        selectedSheetID:'',
+        sheetQueryResult:[]
+    };
+  },
+  computed: mapGetters({})
+};
 </script>
 
 
